7626956c4fdef86858985f7190c4c4534e38755d
XantX <hp_xantx@hotmail.com>
2023-01-17 17:06:14 -0500

    Configuracion de git line blame

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -5,5 +5,13 @@ if not ok then
 end
 
 gitsings.setup {
-  signcolumn = true
+  signcolumn = true,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = '[GitBlame] <author>, <author_time:%Y-%m-%d> - <summary>',
 }
