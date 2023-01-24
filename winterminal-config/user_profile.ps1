b0381962292826fdde7eb890b0aa7a93f0acffee
XantX <hp_xantx@hotmail.com>
2023-01-24 00:01:17 -0500

    Script para listar las ramas

--- winterminal-config/user_profile.ps1
+++ winterminal-config/user_profile.ps1
@@ -1,7 +1,12 @@
 #Alias
+function configWorkGitProfile { 
+  git config --local user.name "Sebastian Diaz" &
+  git config --local user.email "sebastian.diaz.pe@cloudappi.net"}
+function listAll { Get-ChildItem -Force }
+function nvimrc  { set-location "C:\Users\XantX\AppData\Local\nvim\" }
 Set-Alias vim nvim
 Set-Alias ll ls
-function listAll { Get-ChildItem -Force }
+Set-Alias gcnf configWorkGitProfile
 Set-Alias la listAll
 Set-Alias g git
 Set-Alias grep findstr
@@ -9,5 +14,13 @@ Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias touch New-item
 Set-Alias p pwd
-function nvimrc  { set-location "C:\Users\hp_Xa\AppData\Local\nvim\" }
 Set-Alias nvrc nvimrc
+function openFolderOfWaima { set-location 'D:\Trabajo\cloudappi\proyectos\waima\' }
+Set-Alias waima openFolderOfWaima
+
+function gitCheckoutBranch { 
+  $branch=(git branch | fzf --height 40%) 
+  git checkout $branch.Trim()
+}
+
+Set-Alias gtc gitCheckoutBranch
