482d3a1becb9066c5b043607b309c13ae5429d55
XantX <hp_xantx@hotmail.com>
2023-02-07 15:28:36 -0500

    Actualizacion del script para uso de git

--- winterminal-config/user_profile.ps1
+++ winterminal-config/user_profile.ps1
@@ -17,39 +17,43 @@ Set-Alias p pwd
 Set-Alias nvrc nvimrc
 function openFolderOfWaima { set-location 'D:\Trabajo\cloudappi\proyectos\waima\' }
 Set-Alias waima openFolderOfWaima
+function goToDesa { set-location 'D:\desarrollo\'}
+Set-Alias desa goToDesa
 
 function gitCheckoutBranch { 
-  $result=(git branch | fzf --height 50%).Trim()
-  if($result) {
-     $branch= $result.IndexOf("*")
-     if($branch -eq -1) {
-        git checkout $result
-     } else {
-    }
+  $selectedBranch=(git branch | fzf --height 50%)
+  if($selectedBranch) {
+     $selectedBranch = $selectedBranch.Trim()
+     $isActualBranch= $selectedBranch.IndexOf("*")
+     if($isActualBranch -eq -1) {
+        git checkout $selectedBranch
+     }
   }
 }
 
 Set-Alias gtc gitCheckoutBranch
 
 function gitDeleteBranch {
-  $result=(git branch | fzf --height 50%).Trim()
-  if($result) {
-    $branch= $result.IndexOf("*")
-    if($branch -eq -1) {
+  $selectedBranch=(git branch | fzf --height 50%)
+  if($selectedBranch) {
+    $selectedBranch = $selectedBranch.Trim()
+    $actualBranch= $selectedBranch.IndexOf("*")
+    if($actualBranch -eq -1) {
       do {
-        $respuesta = Read-Host "Esta seguro que quiere eliminar la rama local: $result (S/N)"
-        if($respuesta) {
-          switch ( $respuesta ){
-            'S' { git branch -d $result }
+        $response = Read-Host "Esta seguro que quiere eliminar la rama local: $result (S/N)"
+        if($response) {
+          switch ( $response ){
+            'S' { git branch -d $selectedBranch }
             'N' {  }
             default { }
           }
         }
-      }until($respuesta -eq 'S' -or $respuesta -eq 'N')
-
-      #git branch -d $result.Trim()
-    } else {}
+      }until($response -eq 'S' -or $response -eq 'N')
+    }
   }
 }
-
+function showGitBranches {git branch}
+function showGitLogOneLine {git log --graph --oneline}
 Set-Alias gdb gitDeleteBranch
+Set-Alias gb showGitBranches 
+Set-Alias gll showGitLogOneLine
