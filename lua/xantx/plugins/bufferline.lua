b575633570ccd47e9e2c302e3380d5b6dd25145d
XantX <hp_xantx@hotmail.com>
2023-08-30 20:46:34 -0500

    Configuracion de colores para bufferline

--- lua/xantx/plugins/bufferline.lua
+++ lua/xantx/plugins/bufferline.lua
@@ -6,10 +6,58 @@ return {
     { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
   },
   config = function ()
+    local definedBgSelected = "#ebbcba"
+    local definedFgSelected = "#1f1d2e"
     require('bufferline').setup({
+      highlights = {
+          buffer_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+              bold = true,
+              italic = true,
+          },
+          tab_separator_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+          },
+          close_button_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+          },
+          diagnostic_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+              bold = true,
+              italic = true,
+          },
+          hint_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+              bold = true,
+              italic = true,
+          },
+          info_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+              bold = true,
+              italic = true,
+          },
+          separator_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+          },
+          indicator_selected = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+          },
+          offset_separator = {
+              fg = definedFgSelected,
+              bg = definedBgSelected,
+          },
+      },
       options = {
         diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
+        always_show_bufferline = true,
         offsets = {
           {
             filetype = "neo-tree",
