c909ef660729797ef82ae8d694359bff9511c900
XantX <hp_xantx@hotmail.com>
2024-02-23 20:13:28 -0500

    Autopair config

--- lua/xantx/plugins/lsp.lua
+++ lua/xantx/plugins/lsp.lua
@@ -29,8 +29,12 @@ return {
         end,
       }
     })
-
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
+    cmp.event:on(
+      'confirm_done',
+      cmp_autopairs.on_confirm_done()
+    )
     cmp.setup({
         snippet = {
           expand = function(args)
