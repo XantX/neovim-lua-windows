fe6e4affc8e61df273a11b701de9f251d41ef606
XantX <hp_xantx@hotmail.com>
2024-04-01 13:55:33 -0500

    Configuration for work with java

---
+++ lua/xantx/plugins/java-server.lua
@@ -0,0 +1,39 @@
+return {
+  'nvim-java/nvim-java',
+  dependencies = {
+    'nvim-java/lua-async-await',
+    'nvim-java/nvim-java-core',
+    'nvim-java/nvim-java-test',
+    'nvim-java/nvim-java-dap',
+    'MunifTanjim/nui.nvim',
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    {
+      'williamboman/mason.nvim',
+      opts = {
+        registries = {
+          'github:nvim-java/mason-registry',
+          'github:mason-org/mason-registry',
+        },
+      },
+    }
+  },
+  config = function ()
+    require('java').setup()
+    require('lspconfig').jdtls.setup({
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-17",
+                path = "C:/Users/XantX/.jdks/azul-17.0.9",
+                default = true,
+              }
+            }
+          }
+        }
+      }
+    })
+  end
+}
